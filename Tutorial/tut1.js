0ba643ebf19c2888fc3ae7e390bbe899b7f228dc
hrutikumbarkar <123357875+hrutikumbarkar@users.noreply.github.com>
2023-09-28 06:24:45 +0000

    container config done

--- Tutorial/tut1.js
+++ Tutorial/tut1.js
@@ -1,1 +1,6 @@
-console.log("IAS HRUTIK");
+const accountId = 141414
+let accountEmail = "hrutikumbarkar777@gmail.com"
+var accountpassword = "1212121"
+accountAddress = "aurangabad"
+
+console.table([accountId,accountEmail,accountpassword,accountAddress])
