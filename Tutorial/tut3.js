e250908831a0f9969d319c8ac980f30977a3522e
hrutikumbarkar <123357875+hrutikumbarkar@users.noreply.github.com>
2023-09-29 08:55:08 +0000

    conversion of data types

---
+++ Tutorial/tut3.js
@@ -0,0 +1,20 @@
+//conversion operation
+
+//1.number conversion to string
+ let num = 100
+
+ let num_conversion = String(num)
+console.log( typeof num_conversion)
+
+
+
+ //2.boolean conversion 
+ let num1= true
+ let num_boolean = Number(num1)
+ console.log( typeof num_boolean)
+
+//3.string conversion
+
+let str = "MY NAME IS HRUTIK"
+let mystring = Boolean(str)
+console.log(  mystring)
