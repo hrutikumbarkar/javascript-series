73aa4853be0e79c47f71a33c5dbfcb88b1fd165e
hrutikumbarkar <123357875+hrutikumbarkar@users.noreply.github.com>
2023-09-29 12:36:27 +0000

    conversion

--- Tutorial/tut3.js
+++ Tutorial/tut3.js
@@ -16,5 +16,33 @@ console.log( typeof num_conversion)
 //3.string conversion
 
 let str = "MY NAME IS HRUTIK"
-let mystring = Boolean(str)
+let mystring = Boolean(str) 
 console.log(  mystring)
+
+
+//opeartion 
+
+let a=10
+let b=15
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+
+let fname= "hrutik"
+let lname = " umbarkar"
+
+console.log(fname+lname)
+
+//comparison
+
+// console.log("5"==5)
+// console.log("10">=5)
+console.log(null==0)
+console.log(null>=0)
+console.log(null<=0)
+
+console.log(undefined==0)
+
