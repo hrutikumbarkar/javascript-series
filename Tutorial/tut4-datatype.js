703b4bf1048cef2a23e29f8e0c67e52cde1e7ab7
hrutikumbarkar <123357875+hrutikumbarkar@users.noreply.github.com>
2023-09-30 03:27:24 +0000

    data types

---
+++ Tutorial/tut4-datatype.js
@@ -0,0 +1,30 @@
+//Primitive datatypes (call by value)
+//1.number
+
+let num = 100
+console.log(num)
+console.log(" num is a type of ="  + typeof num)
+//2.string
+let str="hrutik"
+console.log(str)
+console.log( "str is a type of ="  + typeof str)
+//3.boolean
+let userLogin = true
+console.log(userLogin)
+console.log(" userlogin type of ="  + typeof userLogin)
+//4.null
+let tempreture = null
+console.log(tempreture)
+console.log(" tempreture type of ="  + typeof tempreture)
+//5.undefined
+let email = undefined
+console.log(email)
+console.log(" email is a type of ="  + typeof email)
+//6.bigInt
+let bigInt = 1010100000n
+console.log(bigInt)
+console.log("largenum is a type of ="  + typeof bigInt)
+//7.symbol
+let id = Symbol('120')
+console.log(id)
+console.log(" symbole is a type of ="  + typeof id)
